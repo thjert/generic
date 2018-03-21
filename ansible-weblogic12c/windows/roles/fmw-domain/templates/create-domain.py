923db79a9e6ac8bd3971468b2b7c667c65a3d427
Vagrant Default User <vagrant@acs>
2018-03-21 07:31:34 +0000

    Med denna commit så går det att starta nodemanager i windows PC, men win_shell hänger när processen har startats

--- ansible-weblogic12c/windows/roles/fmw-domain/templates/create-domain.py
+++ ansible-weblogic12c/windows/roles/fmw-domain/templates/create-domain.py
@@ -1,16 +1,18 @@
-domain_name = '{{ domain_name }}'
-weblogic_home = '{{ weblogic_home }}'
-domain_application_home = '{{ applications_home }}/{{ domain_name }}'
-domain_configuration_home = '{{ domains_home }}/{{ domain_name }}'
-node_manager_home = '{{ nodemanager_home }}'
-java_home = '{{ jdk_folder }}'
-middleware_home = '{{ middleware_home }}'
-weblogic_template = weblogic_home + '/common/templates/wls/wls.jar';
+domain_name = '{{ domain_name }}';
+weblogic_home = '{{ weblogic_home }}';
+domain_application_home = '{{ applications_home }}/{{ domain_name }}';
+domain_configuration_home = '{{ domains_home }}/{{ domain_name }}';
+node_manager_home = '{{ nodemanager_home }}';
+java_home = '{{ jdk_folder }}';
+middleware_home = '{{ middleware_home }}';
+#weblogic_template = weblogic_home + '/common/templates/wls/wls.jar';
+weblogic_template = '{{ oracle_base }}' + '/common/templates/domains/wls.jar';
 
-em_template=middleware_home + '/em/common/templates/wls/oracle.em_wls_template.jar';
+### Används ej Thomas Hjert - em_template=middleware_home + '/em/common/templates/wls/oracle.em_wls_template.jar';
 
 # Load the default domain template
 readTemplate(weblogic_template);
+#
 setOption('DomainName', domain_name);
 setOption('OverwriteDomain', 'true');
 setOption('ServerStartMode', 'prod');
@@ -30,7 +32,8 @@ readDomain(domain_configuration_home);
 
 print 'Add Template';
 # addTemplate(em_template);
-setOption('AppDir', domain_application_home);
+# Ställer troligtvis till det ...
+# setOption('AppDir', domain_application_home);
 
 cd("/SecurityConfiguration/" + domain_name);
 cmo.setNodeManagerUsername('{{ nodemanager_username }}');
@@ -45,8 +48,9 @@ cmo.setTwoWaySSLEnabled(false);
 cmo.setClientCertificateEnforced(false);
 
 cd('/SecurityConfiguration/'+ domain_name +'/Realms/myrealm');
-cd('AuthenticationProviders/DefaultAuthenticator');
-set('ControlFlag', 'SUFFICIENT');
+### Thomas Hjert - DefaultAuthenticator finns ej, måste skapas OM den absolut behövs.
+###cd('AuthenticationProviders/DefaultAuthenticator');
+###set('ControlFlag', 'SUFFICIENT');
 cd('../../');
 # set('Arguments','-Xms=2048m -Xmx=2048m -XX:NewSize=512M -XX:MaxNewSize=512M -XX:PermSize=256M -XX:MaxPermSize=256M');
 print "Update domain";
