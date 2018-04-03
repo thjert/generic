dcf67528f39a426d32f0ae73de0347d7c1c252ec
Vagrant Default User <vagrant@acs>
2018-04-03 07:20:36 +0000

    Det här är första commit efter att manuellt ha ändrat SSL till plain för Nodemanager

--- ansible-weblogic12c/linux/roles/create-managed-server/templates/start-managed-server.py
+++ ansible-weblogic12c/linux/roles/create-managed-server/templates/start-managed-server.py
@@ -1,6 +1,7 @@
-connect('{{ weblogic_admin }}', '{{ weblogic_admin_pass }}')
-start('{{ managed_server_name }}')
-
-# nmConnect('{{ nodemanager_username }}', '{{ nodemanager_password }}', '{{ node_manager_address }}', '{{ node_manager_port }}', '{{ domain_name }}');
-# nmStart('{{ managed_server_name }}');
-# nmDisconnect();
+#####connect('{{ weblogic_admin }}', '{{ weblogic_admin_pass }}')
+#####start('{{ managed_server_name }}')
+######## Det verkar vara problem iom att det är plan text authentication ...
+######## WLSTException: Error occurred while performing nmConnect : Cannot connect to Node Manager. : Unrecognized SSL message, plaintext connection?
+nmConnect('{{ nodemanager_username }}', '{{ nodemanager_password }}', '{{ node_manager_address }}', '{{ node_manager_port }}', '{{ domain_name }}');
+nmStart('{{ managed_server_name }}');
+nmDisconnect();
