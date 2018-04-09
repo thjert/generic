1c84df741e20c7e9955924614988246d1d324c89
Vagrant Default User <vagrant@acs>
2018-04-09 08:33:21 +0000

    Med den ändring som jag gjorde i start-managed-server.py så motsvarar detta det jag senare använde när jag körde igång admin server och managed via NY playbook start-as-ms.yml. Det funkar att köra playbook weblogic-install.yml för att installera och få igång allt. När man stoppat via systemctl stop nodemanger så stoppar det nm, as och ms men när man kör systemctl start nodemanger så startas BARA nm. För att få igång as och ms får man då köra playbook start-as-ms.yml Det funkar också att göra deploy genom att köra playbook weblogic-deploy-applications.yml

--- ansible-weblogic12c/linux/roles/start-managed-server/templates/start-managed-server.py
+++ ansible-weblogic12c/linux/roles/start-managed-server/templates/start-managed-server.py
@@ -1,8 +1,4 @@
-###Raderna nedanför är från den som var innan installationen som påbörjades efter 10:05 2018-04-03
-###ADMIN_SERVER_URL = 't3://' + '192.168.56.14' + ':' + '7001';
-###connect('weblogic', 'welcome1', ADMIN_SERVER_URL);
-###connect('{{ weblogic_admin }}', '{{ weblogic_admin_pass }}')
-###start('{{ managed_server_name }}')
-nmConnect('{{ nodemanager_username }}', '{{ nodemanager_password }}', '{{ node_manager_address }}', '{{ node_manager_port }}', '{{ domain_name }}','{{ domain_home }}', '{{ node_manager_type }}');
-nmStart('{{ managed_server_name }}');
-nmDisconnect();
+ADMIN_SERVER_URL = 't3://' + '{{ admin_server_address }}' + ':' + '{{ admin_server_port }}';
+connect('{{ weblogic_admin }}', '{{ weblogic_admin_pass }}', ADMIN_SERVER_URL)
+start('{{ managed_server_name }}')
+disconnect();
